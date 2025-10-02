1d245d00befaeb8e2b7692f718394cc88abbfd7c
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2025-10-02 00:17:53 -0300

    refactor: Atualização das configurações dos hiperparâmetros dos modelos

--- models/models_config.py
+++ models/models_config.py
@@ -4,183 +4,104 @@ from sklearn.ensemble import (
     AdaBoostClassifier,
     ExtraTreesClassifier,
 )
-from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import (
     LogisticRegression,
-    RidgeClassifier,
     SGDClassifier,
-    PassiveAggressiveClassifier,
-    Perceptron,
 )
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from sklearn.naive_bayes import GaussianNB, BernoulliNB
-from sklearn.neural_network import MLPClassifier
-from sklearn.discriminant_analysis import (
-    LinearDiscriminantAnalysis,
-    QuadraticDiscriminantAnalysis,
-)
+from sklearn.svm import SVC
+from sklearn.naive_bayes import BernoulliNB
 from xgboost import XGBClassifier
 
 MODELOS = {
-    "RandomForest": RandomForestClassifier(random_state=42, class_weight="balanced"),
+    "RandomForest": RandomForestClassifier(n_jobs=6, random_state=42, class_weight="balanced"),
     "DecisionTree": DecisionTreeClassifier(random_state=42, class_weight="balanced"),
-    "LogisticRegression": LogisticRegression(random_state=42, max_iter=1500, class_weight="balanced"),
+    "LogisticRegression": LogisticRegression(n_jobs=6,random_state=42, max_iter=1500, class_weight="balanced"),
     "SVC": SVC(random_state=42, class_weight="balanced", probability=True),
     "GradientBoosting": GradientBoostingClassifier(random_state=42),
     "AdaBoost": AdaBoostClassifier(random_state=42),
-    "GaussianNB": GaussianNB(),
-    "MLPClassifier": MLPClassifier(random_state=42, max_iter=1000),
-    "ExtraTrees": ExtraTreesClassifier(random_state=42, class_weight="balanced"),
-    "LinearSVC": LinearSVC(random_state=42, max_iter=2000),
-    "RidgeClassifier": RidgeClassifier(random_state=42, class_weight="balanced"),
-    "SGDClassifier": SGDClassifier(random_state=42, class_weight="balanced"),
-    # Novos Modelos
-    "NuSVC": NuSVC(random_state=42, class_weight="balanced", probability=True),
-    "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
-    "QuadraticDiscriminantAnalysis": QuadraticDiscriminantAnalysis(),
-    "PassiveAggressiveClassifier": PassiveAggressiveClassifier(random_state=42, class_weight="balanced"),
-    "Perceptron": Perceptron(random_state=42, class_weight="balanced"),
+    "ExtraTrees": ExtraTreesClassifier(n_jobs=6,random_state=42, class_weight="balanced"),
+    "SGDClassifier": SGDClassifier(n_jobs=6,random_state=42, class_weight="balanced"),
     "BernoulliNB": BernoulliNB(),
-    "ExtraTreeClassifier": ExtraTreeClassifier(random_state=42, class_weight="balanced"),
-    "XGBoost": XGBClassifier(random_state=42)
+    "XGBoost": XGBClassifier(n_jobs=6,random_state=42)
 }
 
 PARAM_GRIDS = {
-    "RandomForest": {
-        'n_estimators': [100, 200, 300, 500],
-        'max_depth': [5, 10, 20, 30, None],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
-        'max_features': ['auto', 'sqrt', 'log2', None],
-        'bootstrap': [True, False],
-        'class_weight': ['balanced', 'balanced_subsample', None]
+    "RandomForest": lambda trial: {
+        'n_estimators': trial.suggest_categorical('n_estimators', [100, 200, 300, 500]),
+        'max_depth': trial.suggest_categorical('max_depth', [5, 10, 20, 30]),
+        'min_samples_split': trial.suggest_categorical('min_samples_split', [2, 5, 10]),
+        'min_samples_leaf': trial.suggest_categorical('min_samples_leaf', [1, 2, 4]),
+        'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
+        'bootstrap': trial.suggest_categorical('bootstrap', [True, False]),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced', 'balanced_subsample']),
+        'n_jobs': 6
     },
-    "DecisionTree": {
-        'max_depth': [5, 10, 20, 30, None],
-        'min_samples_leaf': [1, 2, 4, 8],
-        'min_samples_split': [2, 5, 10],
-        'criterion': ['gini', 'entropy', 'log_loss'],
-        'splitter': ['best', 'random'],
-        'class_weight': ['balanced', None]
+    "DecisionTree": lambda trial: {
+        'max_depth': trial.suggest_categorical('max_depth', [5, 10, 20, 30]),
+        'min_samples_leaf': trial.suggest_categorical('min_samples_leaf', [1, 2, 4, 8]),
+        'min_samples_split': trial.suggest_categorical('min_samples_split', [2, 5, 10]),
+        'criterion': trial.suggest_categorical('criterion', ['gini', 'entropy', 'log_loss']),
+        'splitter': trial.suggest_categorical('splitter', ['best', 'random']),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced'])
     },
-    "LogisticRegression": {
-        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
-        'solver': ['lbfgs', 'saga', 'liblinear', 'newton-cg'],
-        'penalty': ['l2', 'l1', 'elasticnet', 'none'],
-        'max_iter': [500, 1000, 1500, 2000],
-        'class_weight': ['balanced', None]
+    "LogisticRegression": lambda trial: {
+        'C': trial.suggest_categorical('C', [0.01, 0.1, 1.0, 10.0, 100.0]),
+        'solver': trial.suggest_categorical('solver', ['lbfgs', 'saga', 'liblinear', 'newton-cg']),
+        'penalty': trial.suggest_categorical('penalty', ['l2', 'l1', 'elasticnet']),
+        'max_iter': trial.suggest_categorical('max_iter', [500, 1000, 1500, 2000]),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced']),
+        'n_jobs': 6
     },
-    "SVC": {
-        'C': [0.01, 0.1, 1, 10, 100],
-        'gamma': ['scale', 'auto', 0.01, 0.1, 1],
-        'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-        'degree': [2, 3, 4],
-        'class_weight': ['balanced', None],
-        'probability': [True]
+    "SVC": lambda trial: {
+        'C': trial.suggest_categorical('C', [0.01, 0.1, 1, 10, 100]),
+        'gamma': trial.suggest_categorical('gamma', ['scale', 'auto', 0.01, 0.1, 1]),
+        'kernel': trial.suggest_categorical('kernel', ['rbf', 'linear', 'poly', 'sigmoid']),
+        'degree': trial.suggest_categorical('degree', [2, 3, 4]),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced']),
+        'probability': True
     },
-    "GradientBoosting": {
-        'n_estimators': [100, 200, 300],
-        'learning_rate': [0.01, 0.05, 0.1, 0.2],
-        'max_depth': [3, 5, 10, 20, None],
-        'subsample': [0.7, 0.8, 1.0],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
-        'max_features': ['auto', 'sqrt', 'log2', None]
+    "GradientBoosting": lambda trial: {
+        'n_estimators': trial.suggest_categorical('n_estimators', [100, 200, 300]),
+        'learning_rate': trial.suggest_categorical('learning_rate', [0.01, 0.05, 0.1, 0.2]),
+        'max_depth': trial.suggest_categorical('max_depth', [3, 5, 10, 20]),
+        'subsample': trial.suggest_categorical('subsample', [0.7, 0.8, 1.0]),
+        'min_samples_split': trial.suggest_categorical('min_samples_split', [2, 5, 10]),
+        'min_samples_leaf': trial.suggest_categorical('min_samples_leaf', [1, 2, 4]),
+        'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2'])
     },
-    "AdaBoost": {
-        'n_estimators': [50, 100, 200, 300],
-        'learning_rate': [0.01, 0.1, 0.5, 1.0, 2.0],
-        'algorithm': ['SAMME', 'SAMME.R']
+    "AdaBoost": lambda trial: {
+        'n_estimators': trial.suggest_categorical('n_estimators', [50, 100, 200, 300]),
+        'learning_rate': trial.suggest_categorical('learning_rate', [0.01, 0.1, 0.5, 1.0, 2.0]),
+        'algorithm': trial.suggest_categorical('algorithm', ['SAMME', 'SAMME.R'])
     },
-    "GaussianNB": {
-        'var_smoothing': [1e-9, 1e-8, 1e-7, 1e-6, 1e-5]
+    "ExtraTrees": lambda trial: {
+        'n_estimators': trial.suggest_categorical('n_estimators', [100, 200, 300, 500]),
+        'max_depth': trial.suggest_categorical('max_depth', [5, 10, 20, 30]),
+        'min_samples_split': trial.suggest_categorical('min_samples_split', [2, 5, 10]),
+        'min_samples_leaf': trial.suggest_categorical('min_samples_leaf', [1, 2, 4]),
+        'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
+        'bootstrap': trial.suggest_categorical('bootstrap', [True, False]),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced', 'balanced_subsample']),
+        'n_jobs': 6
     },
-    "MLPClassifier": {
-        'hidden_layer_sizes': [(50,), (100,), (50, 50), (100, 50), (100, 100)],
-        'activation': ['relu', 'tanh', 'logistic'],
-        'solver': ['adam', 'sgd', 'lbfgs'],
-        'alpha': [0.0001, 0.001, 0.01],
-        'learning_rate': ['constant', 'invscaling', 'adaptive'],
-        'max_iter': [500, 1000, 2000]
+    "SGDClassifier": lambda trial: {
+        'alpha': trial.suggest_categorical('alpha', [0.0001, 0.001, 0.01, 0.1]),
+        'penalty': trial.suggest_categorical('penalty', ['l2', 'l1', 'elasticnet']),
+        'loss': trial.suggest_categorical('loss', ['hinge', 'log_loss', 'modified_huber', 'squared_hinge']),
+        'max_iter': trial.suggest_categorical('max_iter', [1000, 2000, 3000]),
+        'class_weight': trial.suggest_categorical('class_weight', ['balanced']),
+        'n_jobs': 6
     },
-    "ExtraTrees": {
-        'n_estimators': [100, 200, 300, 500],
-        'max_depth': [5, 10, 20, 30, None],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
-        'max_features': ['auto', 'sqrt', 'log2', None],
-        'bootstrap': [True, False],
-        'class_weight': ['balanced', 'balanced_subsample', None]
-    },
-    "LinearSVC": {
-        'C': [0.01, 0.1, 1, 10, 100],
-        'penalty': ['l2'],
-        'loss': ['squared_hinge'],
-        'max_iter': [1000, 2000, 3000],
-        'class_weight': ['balanced', None]
-    },
-    "RidgeClassifier": {
-        'alpha': [0.01, 0.1, 1.0, 10.0, 100.0],
-        'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sag', 'saga'],
-        'class_weight': ['balanced', None]
-    },
-    "SGDClassifier": {
-        'alpha': [0.0001, 0.001, 0.01, 0.1],
-        'penalty': ['l2', 'l1', 'elasticnet'],
-        'loss': ['hinge', 'log_loss', 'modified_huber', 'squared_hinge'],
-        'max_iter': [1000, 2000, 3000],
-        'class_weight': ['balanced', None]
-    },
-    # Grades para Novos Modelos
-    "NuSVC": {
-        'nu': [0.1, 0.3, 0.5, 0.7, 0.9],
-        'gamma': ['scale', 'auto', 0.01, 0.1, 1],
-        'kernel': ['rbf', 'linear', 'poly', 'sigmoid'],
-        'degree': [2, 3, 4],
-        'class_weight': ['balanced', None],
-        'probability': [True]
-    },
-    "LinearDiscriminantAnalysis": {
-        'solver': ['svd', 'lsqr', 'eigen'],
-        'shrinkage': [None, 'auto', 0.1, 0.5, 1.0]
-    },
-    "QuadraticDiscriminantAnalysis": {
-        'reg_param': [0.0, 0.1, 0.3, 0.5, 0.7, 1.0],
-        'store_covariance': [True, False],
-        'tol': [1e-4, 1e-3, 1e-2]
-    },
-    "PassiveAggressiveClassifier": {
-        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
-        'loss': ['hinge', 'squared_hinge'],
-        'max_iter': [1000, 2000, 3000],
-        'class_weight': ['balanced', None]
-    },
-    "Perceptron": {
-        'penalty': [None, 'l2', 'l1', 'elasticnet'],
-        'alpha': [0.0001, 0.001, 0.01],
-        'max_iter': [1000, 2000, 3000],
-        'class_weight': ['balanced', None]
-    },
-    "BernoulliNB": {
-        'alpha': [0.01, 0.1, 0.5, 1.0, 2.0],
-        'fit_prior': [True, False]
-    },
-    "ExtraTreeClassifier": {
-        'max_depth': [5, 10, 20, 30, None],
-        'min_samples_leaf': [1, 2, 4, 8],
-        'min_samples_split': [2, 5, 10],
-        'criterion': ['gini', 'entropy', 'log_loss'],
-        'splitter': ['best', 'random'],
-        'class_weight': ['balanced', None]
-    },
-    "XGBoost": {
-        'n_estimators': [100, 200, 300, 500],
-        'max_depth': [3, 5, 10, 20, None],
-        'learning_rate': [0.01, 0.05, 0.1, 0.2],
-        'subsample': [0.7, 0.8, 1.0],
-        'colsample_bytree': [0.7, 0.8, 1.0],
-        'gamma': [0, 0.1, 0.5, 1.0],
-        'reg_alpha': [0, 0.1, 1],
-        'reg_lambda': [0.5, 1, 2]
+    "XGBoost": lambda trial: {
+        'n_estimators': trial.suggest_categorical('n_estimators', [100, 200, 300, 500]),
+        'max_depth': trial.suggest_categorical('max_depth', [3, 5, 10, 20]),
+        'learning_rate': trial.suggest_categorical('learning_rate', [0.01, 0.05, 0.1, 0.2]),
+        'subsample': trial.suggest_categorical('subsample', [0.7, 0.8, 1.0]),
+        'colsample_bytree': trial.suggest_categorical('colsample_bytree', [0.7, 0.8, 1.0]),
+        'gamma': trial.suggest_categorical('gamma', [0, 0.1, 0.5, 1.0]),
+        'reg_alpha': trial.suggest_categorical('reg_alpha', [0, 0.1, 1]),
+        'reg_lambda': trial.suggest_categorical('reg_lambda', [0.5, 1, 2]),
+        'n_jobs': 6
     }
 }
